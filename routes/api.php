d43d3aeb1cc8b7a679dd7c2fb2f5d8815352f225
rachmanj <rachmanj@gmail.com>
2023-01-31 14:22:11 +0800

    fitur api wait payment

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\InvoiceApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::put('/invoices/{id}', [InvoiceApiController::class, 'update'])->name('invoices.update');
+Route::get('/invoices/{id}', [InvoiceApiController::class, 'show'])->name('invoices.show');
+Route::post('/invoices', [InvoiceApiController::class, 'test'])->name('invoices.test');
