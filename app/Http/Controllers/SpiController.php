cb0151905aa24d5d2bb370e4347b59a7982c330e
rachmanj <rachmanj@gmail.com>
2023-02-02 14:31:54 +0800

    update spi sent to payreq

--- app/Http/Controllers/SpiController.php
+++ app/Http/Controllers/SpiController.php
@@ -30,36 +30,54 @@ class SpiController extends Controller
         $spi = Spi::find($spi_id);
         $spi->update([
             'received_at' => $request->received_date,
-            'received_by' => auth()->user()->name
+            'received_by' => auth()->user()->username
         ]);
 
         $bpn_project_id = Project::where('project_code', '000H')->first()->project_id;
-        
+
         if (auth()->user()->projects_id === $bpn_project_id) {  // kalo user project 000H
-            Invoice::where('spis_id', $spi->id)->update([
-                'spi_bpn_date' => $request->received_date
-            ]);
+            $invoices = Invoice::where('spis_id', $spi->id)->get();
+            // sent api to Payreq-system post invoice
+            foreach ($invoices as $invoice) {
+                $url = 'http://localhost:8001/api/invoices';
+                $data = [
+                    "nomor_invoice" => $invoice->inv_no,
+                    "invoice_irr_id" => $invoice->inv_id,
+                    "vendor_name" => $invoice->vendor->vendor_name,
+                    "received_date" => $invoice->receive_date,
+                    "amount" => $invoice->inv_nominal,
+                    "remarks" => $invoice->remarks ? $invoice->remarks : '-',
+                    'sender_name' => auth()->user()->username
+                ];
+                $client = new \GuzzleHttp\Client();
+                $client->request('POST', $url, [
+                    'form_params' => $data
+                ]);
+                $invoice->spi_bpn_date = $request->received_date;
+                $invoice->senttoacc_id = 1;
+                $invoice->save();
+            }
         } else {     // kalo user selain 000H / balikpapan
             Invoice::where('spis_id', $spi->id)->update([
                 'mailroom_jkt_date' => $request->received_date
             ]);
         }
 
-        return redirect()->route('spis.create.index')->with('success', 'SPI receive updated');
+        return redirect()->route('spis.create.index')->with('success', 'SPI receive updated and invoices send to cashier');
     }
 
     public function index_data()
     {
-        if(auth()->user()->role === 'SUPERADMIN') {
+        if (auth()->user()->role === 'SUPERADMIN') {
             $spis = Spi::whereNull('received_at')
-                    ->orderBy('date', 'desc') //
-                    ->orderBy('nomor', 'desc')
-                    ->get();
+                ->orderBy('date', 'desc') //
+                ->orderBy('nomor', 'desc')
+                ->get();
         } else {
             $bpn_project_id = Project::where('project_code', '000H')->first()->project_id;
             $jkt_project_id = Project::where('project_code', '001H')->first()->project_id;
 
-            if(auth()->user()->projects_id === $bpn_project_id) {
+            if (auth()->user()->projects_id === $bpn_project_id) {
                 $to_projects_id = $bpn_project_id;
             } else {
                 $to_projects_id = $jkt_project_id;
